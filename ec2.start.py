5c2363dff5746c58d84bf8897579f40abe271adf
rauhl <torahulpandey1983@outlook.com>
2021-12-07 15:19:19 +1100

    add the python code to start or stop ec2

--- ec2.start.py
+++ ec2.start.py
@@ -1,6 +1,5 @@
 import boto3
 
-
 client = boto3.client('ec2')
 response = client.describe_instances(
     Filters=[
@@ -15,7 +14,18 @@ response = client.describe_instances(
 
 for r in response['Reservations']:
   for i in r['Instances']:
-     instance = client.stop_instances(InstanceIds=[i['InstanceId']])
-     print("stopped successfully \n ", i['InstanceId'])
+     instance = client.start_instances(InstanceIds=[i['InstanceId']])
+     print("started successfully \n ", i['InstanceId'])
+
+
+'''
+ec2 = boto3.resource('ec2')
+instance = ec2.Instance('i-022e9e3f85ec1fa0c')
+
+result = instance.start()
+print(result)
+
+'''
+
 
 
